4dd0ddf492dc145fd9f3ef276f5bacaea786b824
Rounak Tikadar <rounak.tikadar@gmail.com>
2022-04-13 15:58:44 +0530

    added basic ts node worker

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  'env': {
+    'es2021': true,
+    'node': true,
+  },
+  'extends': [
+    'google',
+  ],
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module',
+  },
+  'plugins': [
+    '@typescript-eslint',
+  ],
+  'rules': {
+    'max-len': ['off'],
+    'require-jsdoc': ['off'], // todo: remove this later when the project is ready for active documentation
+  },
+};
